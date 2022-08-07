1065a3ad3d9f039dd840da786a39aa92f2893df2
Gxy <1002935322@qq.com>
2022-08-07 13:22:39 +0800

    template

---
+++ routers/routes.go
@@ -0,0 +1,18 @@
+package routers
+
+import (
+	"awesomeProject/logger"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetUp() *gin.Engine {
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "ok"})
+	})
+
+	return r
+}
